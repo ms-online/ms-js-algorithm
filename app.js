7a3451c2b035036ee74c3dce950ca2e07d7579f5
ms-online <27732357@qq.com>
2020-08-16 16:31:32 +0800

    lesson-4

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 // 如何返回斐波那契数列中下标为n的元素？
 
 function fib(n) {
-  const numbers = [1, 1];
+  const numbers = [1, 1]; // 1
   for (let i = 2; i < n + 1; i++) {
-    numbers.push(numbers[i - 2] + numbers[i - 1]);
+    // 1
+    console.log('123'); // n -1
+    counts++;
+    numbers.push(numbers[i - 2] + numbers[i - 1]); // n -1
   }
-  console.log(numbers);
-  return numbers[n];
+  // console.log(numbers);
+  return numbers[n]; //1
 }
 
+// T = 1 + 1 + 2*(n - 1) + 1 = 1 + 2*n
+// T = 2*n
+// T = n => O(n) => 线性时间复杂度
+
 console.log(fib(4));
-console.log(fib(5));
-console.log(fib(6));
