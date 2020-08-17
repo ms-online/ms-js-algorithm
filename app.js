60ba6e263df828e4eedf06f54072d6056ec95fa8
ms-online <27732357@qq.com>
2020-08-17 15:05:42 +0800

    lesson-7

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // 判断一个输入数字是否为质数?
 
 function isPrime(number) {
-  for (let i = 2; i < number; i++) {
+  for (let i = 2; i < Math.sqrt(number); i++) {
     // 1
     console.log('123'); // n
     if (number % i === 0) {
@@ -13,8 +13,8 @@ function isPrime(number) {
 }
 
 // 最好情况： number = 1 或者 number 2 => T = 1 + 1  = 2 => O(1) 常数时间复杂度
-// 平均情况： O(n) 线性时间复杂度
-// 最差情况： number = 27277 => T = n => O(n) 线性时间复杂度
+// 平均情况： O(n) 线性时间复杂度 => 性能提升：O(sqrt(n))
+// 最差情况： number = 27277 => T = n => O(n) => 性能提升：O(sqrt(n))
 
 // console.log(isPrime(1));
 // console.log(isPrime(2));
