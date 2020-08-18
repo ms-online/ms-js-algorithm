ef82342c65515370283e7f4ff37d3c53aa063ee6
ms-online <27732357@qq.com>
2020-08-18 15:40:34 +0800

    lesson-12

--- app.js
+++ app.js
@@ -1,42 +1,13 @@
-// function isPowerOfTwo(number) {
-//   if (number < 1) {
-//     return false;
-//   }
-// if (number % 2 !== 0) {
-//   return false;
-// }
-// return true;
-// 12 / 2 = 6
-// 6 / 2 = 3
-// 3 / 2 = 1 ··· 1（余数）
-
-//   let dividedNumber = number;
-//   while (dividedNumber !== 1) {
-//     if (dividedNumber % 2 !== 0) {
-//       return false;
-//     }
-//     dividedNumber = dividedNumber / 2;
-//   }
-//   return true;
-// }
-
-// 最好情况： 13 => O(1);
-// 平均情况：O(log n)
-// 最差情况： 1125899906842624 => O(log n) 对数时间复杂度
-
-// 第二种方法 按位与运算 &
-function isPowerOfTwo(number) {
-  if (number < 1) {
-    return false;
+function fact(number) {
+  let result = 1; // 0的阶乘为1；
+  for (let i = 2; i <= number; i++) {
+    result = result * i;
   }
-
-  return (number & (number - 1)) === 0; // 1
+  return result;
 }
 
-// T : O(1) 常数时间复杂度
+// T : O(n);
 
-console.log(isPowerOfTwo(5));
-console.log(isPowerOfTwo(8));
-console.log(isPowerOfTwo(12));
-console.log(isPowerOfTwo(13));
-console.log(isPowerOfTwo(16));
+console.log(fact(3));
+console.log(fact(4));
+console.log(fact(5));
