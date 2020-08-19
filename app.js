7628b5741c8172724caabb1bcdc14b7640069095
ms-online <27732357@qq.com>
2020-08-19 19:58:54 +0800

    lesson-16

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 let counter = 0; // 计算函数调用次数
-function fib(n) {
+function fib(n, memo) {
+  let result; // 存储中间值
   counter++;
-  // 3
+  if (memo[n]) return memo[n]; // 验证这个中间值是否存在，若存在就返回结果，不存在就跳过执行下方代码。
   if (n === 0 || n === 1) {
-    // [1,1,2,3,5,8]
-    return 1;
+    result = 1;
+  } else {
+    result = fib(n - 1, memo) + fib(n - 2, memo);
   }
-  return fib(n - 1) + fib(n - 2);
+  memo[n] = result;
+  return result;
 }
 
 // n =3 => 3
@@ -16,21 +19,21 @@ function fib(n) {
 
 // O(2^n) 指数时间复杂度
 
-// console.log(fib(5));
-// console.log(fib(36));
+// console.log(fib(5, {})); // 空对象的原因：在递归的过程中，重复中间值才会被存储在对象里面
+// console.log(fib(6, {}));
 
-fib(5);
+fib(5, {});
 console.log(counter);
 counter = 0;
 
-fib(10);
+fib(10, {});
 console.log(counter);
 counter = 0;
 
-fib(20);
+fib(20, {});
 console.log(counter);
 counter = 0;
 
-fib(30);
+fib(50, {});
 console.log(counter);
 counter = 0;
