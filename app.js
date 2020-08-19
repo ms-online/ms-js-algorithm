1ee5bb39db0f7a83fc6b742df1bec4f1da6edbb6
ms-online <27732357@qq.com>
2020-08-19 15:55:04 +0800

    lesson-15

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+let counter = 0; // 计算函数调用次数
 function fib(n) {
+  counter++;
   // 3
   if (n === 0 || n === 1) {
     // [1,1,2,3,5,8]
@@ -12,7 +14,23 @@ function fib(n) {
 // fib(1) + fib(0) + 1
 // 1 + 1 + 1 = 3
 
-// O(2^n)
+// O(2^n) 指数时间复杂度
 
-console.log(fib(5));
-console.log(fib(36));
+// console.log(fib(5));
+// console.log(fib(36));
+
+fib(5);
+console.log(counter);
+counter = 0;
+
+fib(10);
+console.log(counter);
+counter = 0;
+
+fib(20);
+console.log(counter);
+counter = 0;
+
+fib(30);
+console.log(counter);
+counter = 0;
