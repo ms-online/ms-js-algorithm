34ba043ac92410373226b30d7f0e456b62a31111
ms-online <27732357@qq.com>
2020-08-23 16:51:38 +0800

    lesson-13

--- app.js
+++ app.js
@@ -8,6 +8,7 @@
 // }
 
 // T : O(n);
+// 空间复杂度：O(1) 常数空间复杂度
 
 // 递归实现阶乘问题
 function fact(number) {
@@ -28,6 +29,8 @@ function fact(number) {
 // 当触发多个被调用的函数的时候，n个被调用的函数
 // T = n * O(1) => O(n);
 // T = 1 * n = n => O(n);
+
+//空间复杂度：O(n) 线性空间复杂度
 console.log(fact(3)); // 3 * 2 * 1 = 6;
 console.log(fact(4)); // 4 * 3 * 2 * 1 = 24;
 console.log(fact(5)); // 5 * 4 * 3 * 2 * 1 = 120;
