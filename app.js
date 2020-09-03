ffa18ce7dcf3624d629992ca71941329d2faf40d
ms-online <27732357@qq.com>
2020-09-03 17:26:51 +0800

    lesson-30

--- app.js
+++ app.js
@@ -1,45 +1,24 @@
-function knapsack(items, cap, itemIndex) {
-  //跳出递归
-  if (cap === 0 || itemIndex < 0) {
-    return { items: [], value: 0, weight: 0 };
-  }
-  //物品是否大于背包最大承载量
-  if (cap < items[itemIndex].weight) {
-    return knapsack(items, cap, itemIndex - 1);
-  }
+function knapsack(elements, capacity) {
+  const sack = { items: [], value: 0, weight: 0 };
+  let remainingCapacity = capacity;
 
-  //放入物品
-  const sackWithItem = knapsack(
-    items,
-    cap - items[itemIndex].weight,
-    itemIndex - 1
-  );
-  //不放入物品
-  const sackWithoutItem = knapsack(items, cap, itemIndex - 1);
-  //获取价值
-  const valueWithItem = sackWithItem.value + items[itemIndex].value;
-  const valueWithoutItem = sackWithoutItem.value;
-
-  //价值比较
-  if (valueWithItem > valueWithoutItem) {
-    const updateSack = {
-      items: sackWithItem.items.concat(items[itemIndex]),
-      value: sackWithItem.value + items[itemIndex].value,
-      weight: sackWithItem.weight + items[itemIndex].weight,
-    };
-
-    return updateSack;
-  } else {
-    return sackWithoutItem;
+  for (const element of elements) {
+    if (element.weight <= remainingCapacity) {
+      sack.items.push(element);
+      sack.value += element.value;
+      sack.weight += element.weight;
+      remainingCapacity -= element.weight;
+    }
   }
+  return sack;
 }
 const items = [
   { name: 'a', value: 5, weight: 4 },
-  { name: 'b', value: 8, weight: 10 },
+  { name: 'b', value: 8, weight: 8 },
   { name: 'c', value: 6, weight: 3 },
 ];
 
-const maxCap = 10;
+const maxCap = 8;
 
-const sack = knapsack(items, maxCap, items.length - 1);
+const sack = knapsack(items, maxCap);
 console.log(sack);
