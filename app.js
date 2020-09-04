0413bc3c044cd15a59c3ed4e028a4cec669c7931
ms-online <27732357@qq.com>
2020-09-04 16:16:20 +0800

    lesson-31

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
-function knapsack(elements, capacity) {
-  const sack = { items: [], value: 0, weight: 0 };
-  let remainingCapacity = capacity;
+function computeChange(coins, amount) {
+  let remainingAmount = amount;
+  const calculatedChange = {
+    selectedCoins: {},
+    numberOfCoins: 0,
+  };
+  for (const coin of coins) {
+    const count = Math.floor(remainingAmount / coin);
+    calculatedChange.selectedCoins[coin] = count;
+    calculatedChange.numberOfCoins += count;
+    remainingAmount = remainingAmount - coin * count;
+  }
+  return calculatedChange;
+}
 
-  for (const element of elements) {
-    if (element.weight <= remainingCapacity) {
-      sack.items.push(element);
-      sack.value += element.value;
-      sack.weight += element.weight;
-      remainingCapacity -= element.weight;
+function computeChangeBruteForce(coins, amount) {
+  const results = [];
+  for (let i = 0; i < coins.length; i++) {
+    results.push(computeChange(coins.slice(i), amount));
+  }
+
+  let smallestAmountCoins = Number.MAX_SAFE_INTEGER;
+  let finalResult;
+  for (const result of results) {
+    if (result.numberOfCoins < smallestAmountCoins) {
+      smallestAmountCoins = result.numberOfCoins;
+      finalResult = result;
     }
   }
-  return sack;
+  return finalResult;
 }
-const items = [
-  { name: 'a', value: 5, weight: 4 },
-  { name: 'b', value: 8, weight: 8 },
-  { name: 'c', value: 6, weight: 3 },
-];
 
-const maxCap = 8;
+const availableCoins = [8, 6, 5, 1];
+const targetAmount = 63;
 
-const sack = knapsack(items, maxCap);
-console.log(sack);
+const change = computeChangeBruteForce(availableCoins, targetAmount);
+console.log(change);
+
+//时间复杂度 （贪婪算法）： O(n)
+//时间复杂度 （暴力搜索算法）：O(n*n) + O(n) => O(n^2)
